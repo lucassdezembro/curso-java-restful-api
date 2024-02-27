1c9a29fcdb15a0239cdaad3b0374d31f9de1c20c
Lucas Dezembro <lucas.dezembro@dilettasolutions.com>
2024-02-27 23:07:53 +0000

    adiciona classe de constante de erros

---
+++ src/main/java/br/com/lucassdezembro/errors/ConstantErrors.java
@@ -0,0 +1,6 @@
+package br.com.lucassdezembro.errors;
+
+public class ConstantErrors {
+	public static final String numbersShouldBeString = "Parameters 'number1' or 'number2' aren't numbers.";
+	public static final String numberShouldBeString = "Parameter 'number1' isn't number.";
+}
