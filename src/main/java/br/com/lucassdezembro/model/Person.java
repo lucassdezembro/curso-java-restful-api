614e8bb35e694ce3e72380aa98a761edb689c3a4
Lucas Dezembro <lucas.dezembro@dilettasolutions.com>
2024-03-21 09:35:16 -0300

    adiciona entity e repository para Person

--- src/main/java/br/com/lucassdezembro/model/Person.java
+++ src/main/java/br/com/lucassdezembro/model/Person.java
@@ -3,19 +3,36 @@ package br.com.lucassdezembro.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Person") // quando o nome da classe é o mesmo da tabela, não é necessário usar essa nota
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
+	
+	@Column(name = "last_name", nullable = false, length = 80)
 	private String lastName;
+	
+	@Column(nullable = false, length = 100)
 	private String address;
+	
+	@Column(nullable = false, length = 6)
 	private String gender;
 
-	public Person() {
-
-	}
+	public Person() {}
 
 	public Long getId() {
 		return id;
